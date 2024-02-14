de9dfe06acf5c87fa9f1e61600a08ac74f6aba7e
Mikel Orobengoa <mikelorobe@gmail.com>
2024-02-14 16:01:34 +0100

    Ray reflection

--- Assets/Scripts/RaySource.cs
+++ Assets/Scripts/RaySource.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class RaySource : MonoBehaviour
 {
     // Start is called before the first frame update
+    private LightRay ray;
     void Start()
     {
-        
+        GameObject oRay = new GameObject("Light Ray");
+        oRay.transform.position = transform.position;
+        oRay.transform.rotation = transform.rotation;
+        ray = oRay.AddComponent<LightRay>();
     }
 
     // Update is called once per frame
