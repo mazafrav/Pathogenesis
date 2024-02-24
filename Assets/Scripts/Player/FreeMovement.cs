fd43cf75b877bd0a522cb67d4cc3609632356782
Alonefcp <fcuadra@ucm.es>
2024-02-24 21:10:35 +0100

    Added free movement to the player and create a free movement area

---
+++ Assets/Scripts/Player/FreeMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeMovement : MonoBehaviour
+{
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController playerController = collision.GetComponent<PlayerController>();
+        if(playerController != null)
+        {
+            playerController.SetFreeMovement();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        PlayerController playerController = collision.GetComponent<PlayerController>();
+        if (playerController != null)
+        {
+            playerController.UnsetFreeMovement();
+        }
+    }
+}
