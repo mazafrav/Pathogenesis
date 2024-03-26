bf0794dc9c8ae0fcc7048545877099335379582e
mazafrav <mazafrav@gmail.com>
2024-03-26 13:25:20 +0100

    Added level transitions, Added level 1

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    [SerializeField]
+    public float transitionTime = 0.7f;
+    
+    public void StartLoadingLevel(int level)
+    {
+        StartCoroutine(LoadLevel(level));
+    }
+
+    IEnumerator LoadLevel(int level)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(level);
+    }
+}
