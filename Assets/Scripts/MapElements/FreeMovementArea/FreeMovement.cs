512fa1cdebf5e646481348564467b4a406f098d0
Alonefcp <fcuadra@ucm.es>
2024-04-14 20:21:14 +0200

    Improved Thrust of the Free movement area
    #changelist validated

--- Assets/Scripts/MapElements/FreeMovementArea/FreeMovement.cs
+++ Assets/Scripts/MapElements/FreeMovementArea/FreeMovement.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class FreeMovement : MonoBehaviour
 {
+    [Header("Bounciness avoidance")]
     [SerializeField] private bool canBlockBouncines = false;
     [SerializeField] private GameObject blockingBouncines;
+    [Header("Thrust")]
     [SerializeField] private bool applyThrust = false;
     [SerializeField] private float thrust = 20.0f;
+    [SerializeField] private float time = 1.0f;
+    private bool hasDisabledControls = false;
+    private float currentTime = 0.0f;
 
     private void Start()
     {
+        currentTime = time;
         blockingBouncines.SetActive(false);
         blockingBouncines.GetComponent<BoxCollider2D>().enabled = false;
         blockingBouncines.GetComponent<BlockingBouncines>().enabled = false;
+    }
+
+    private void Update()
+    {
+        if(hasDisabledControls)
+        {
+            currentTime -= Time.deltaTime;
+        }
+        if(currentTime<=0.0f)
+        {
+            currentTime = time;
+            hasDisabledControls = false;
+            PlayerController playerController = GameManager.Instance.GetPlayerController();
+            playerController.enabled = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -38,8 +59,15 @@ public class FreeMovement : MonoBehaviour
             {
                 Vector2 dir = new Vector2(playerController.GetDeltaX(), playerController.GetDeltaY());               
                 collision.GetComponent<Rigidbody2D>().AddForce(dir * thrust, ForceMode2D.Impulse);
+
+                if(dir.x > 0.0f || dir.x < 0.0f)
+                {
+                    playerController.enabled = false;
+                    hasDisabledControls = true;
+                }
             }
             
+            //We block the bouncines that happen when the player goes from bottom to up
             if (canBlockBouncines && playerController.GetDeltaY() > 0.0f)
             {
                 blockingBouncines.SetActive(true);
