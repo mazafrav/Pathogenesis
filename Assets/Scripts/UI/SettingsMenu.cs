60781b0cfcc72f5974ea70ee5c0a5f4f506debaf
envalasen <envalasen@gmail.com>
2024-05-13 11:08:11 +0200

    updated settings menu and possession mat

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
 
     private GameObject eventSystem;
     private GameObject eventSystem1;
@@ -14,6 +21,43 @@ public class SettingsMenu : MonoBehaviour
     {
         eventSystem = GameObject.FindGameObjectWithTag("EventSystemMainMenu");
         eventSystem1 = GameObject.FindGameObjectWithTag("EventSystemSettings");
+
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+     
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetFullscreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
     }
 
     public void Back()
