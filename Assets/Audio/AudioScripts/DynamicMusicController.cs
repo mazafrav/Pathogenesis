81b0beac66a765307afaf423a042b2a156b6b3bc
mazafrav <mazafrav@gmail.com>
2024-05-29 13:02:11 +0200

    Refactor dynamic music controller

--- Assets/Audio/AudioScripts/DynamicMusicController.cs
+++ Assets/Audio/AudioScripts/DynamicMusicController.cs
@@ -3,47 +3,33 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public enum STATES
-{
-    Capa0 = 0,
-    Capa1 = 1,
-    Capa2 = 2,
-    Capa3 = 3,
-    Capa4 = 4,
-    Capa5 = 5,
-}
-
 public class DynamicMusicController : MonoBehaviour
 {
-  //public string[] states = new string[]{"Capa0", "Capa1", "Capa2","Capa3","Capa4","Capa5"} ;//Array of states
-
-  public AudioMixerSnapshot[] tracks = new AudioMixerSnapshot[5];
-  public int selectionIndex = 0;
-  public float transitionSpeed = 0.5f;
-  public STATES currentState;
-  public static DynamicMusicController instance;
-  private void Awake()
-  {
-      if (instance == null)
-      {
-          instance = this;
-          DontDestroyOnLoad(gameObject);
+    public AudioMixerSnapshot[] tracks = new AudioMixerSnapshot[5];
+    [SerializeField]
+    public int selectionIndex = 0;
+    [SerializeField]
+    public float transitionSpeed = 0.5f;
+ 
+    public static DynamicMusicController instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
           
-      }
-      else
-      {
-          Destroy(gameObject);
-      }
-  }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
-  void Update()
-  {
-      if (selectionIndex != (int)currentState)
-      {
-          selectionIndex = (int)currentState;
-          tracks[selectionIndex].TransitionTo(transitionSpeed);
-      }
-  }
+    void Update()
+    {
+        tracks[selectionIndex].TransitionTo(transitionSpeed);
+    }
 }
 
 
