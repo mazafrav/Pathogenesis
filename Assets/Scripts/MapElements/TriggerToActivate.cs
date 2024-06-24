7f7c0c40ffda1db09f6317d87a67db0cd7983bf3
mazafrav <mazafrav@gmail.com>
2024-06-24 20:57:39 +0200

    Added new prefab to activate gameobjects when triggered, merged lvl 4 into 3 (testing)

---
+++ Assets/Scripts/MapElements/TriggerToActivate.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerToActivate : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject[] itemsToActivate;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
+        {
+
+            foreach (GameObject element in itemsToActivate)
+            {
+                if (element != null) { element.SetActive(true); };
+            }
+        }
+    }
+}
