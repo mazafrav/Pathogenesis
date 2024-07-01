769311ea72859804ed8ef8f488d7701f1a27434c
mazafrav <mazafrav@gmail.com>
2024-07-01 09:32:13 +0200

    Added ending cutscene (first iteration), updated lvl 12,13. Added lvl3 to build settings

--- Assets/Scripts/MapElements/StartCutscene.cs
+++ Assets/Scripts/MapElements/StartCutscene.cs
@@ -2,16 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.VFX;
 
 public class StartCutscene : MonoBehaviour
 {
     [SerializeField]
     public static bool isCutscenePlaying = false;
     [SerializeField] PlayableDirector director;
+    [SerializeField]
+    private VisualEffect absortionRangeVfx;
+
+    [SerializeField]
+    private GameObject virus;
+    [SerializeField]
+    private GameObject evilVirus;
 
     private void Awake()
     {
         director.stopped += DirectorStopped;
+    }
+
+    private void Update()
+    {
+        absortionRangeVfx.SetVector3("Direction", (virus.transform.position - evilVirus.transform.position).normalized);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -25,5 +38,11 @@ public class StartCutscene : MonoBehaviour
     private void DirectorStopped(PlayableDirector obj)
     {
         GameManager.Instance.GetLevelLoader().StartLoadingLevel(0);
+    }
+
+    public void StartVFX()
+    {
+        absortionRangeVfx.SetVector3("Direction", (virus.transform.position - evilVirus.transform.position).normalized);
+        absortionRangeVfx.Play();
     }
 }
