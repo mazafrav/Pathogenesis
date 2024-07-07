022d0c62de85cd9b3a841088b9e9c72358beafc2
Felipe Cuadra Plaza <fcuadra@ucm.es>
2024-07-08 01:28:17 +0200

    Fix: Kinetic receptor activate sound is played

--- Assets/Scripts/MapElements/Receptors/KineticReceptor.cs
+++ Assets/Scripts/MapElements/Receptors/KineticReceptor.cs
@@ -15,13 +15,17 @@ public class KineticReceptor : MonoBehaviour
     [SerializeField]
     private AudioClip activateClip;
 
+    private CircleCollider2D circleCollider;
+    private SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     private void Start()
     {
         activatableInterface = activatableElement.GetComponent<IActivatableElement>();
         if (activatableInterface == null) { throw new System.Exception("Object does not implement IActivaatbleElement"); }
         audioSource = GetComponent<AudioSource>();
-
+        circleCollider = GetComponent<CircleCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void Stabbed()
@@ -30,6 +34,8 @@ public class KineticReceptor : MonoBehaviour
         ParticleSystem bulletVFX = Instantiate(destructionVFX, this.gameObject.transform.position, Quaternion.identity);
         audioSource.PlayOneShot(activateClip);
         bulletVFX.Play();
-        Destroy(this.gameObject);
+        circleCollider.enabled = false;
+        spriteRenderer.enabled = false;
+        Destroy(this.gameObject, activateClip.length);
     }
 }
