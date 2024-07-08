7d1a6b5977dc5ec9a2aa6278eabebf7a4bf71e0a
mazafrav <mazafrav@gmail.com>
2024-07-08 11:52:02 +0200

    Updated camera trigger and lvl9

--- Assets/Scripts/MapElements/CameraTrigger.cs
+++ Assets/Scripts/MapElements/CameraTrigger.cs
@@ -7,39 +7,62 @@ using Unity.VisualScripting;
 
 public class CameraTrigger : MonoBehaviour
 {
-    [SerializeField] bool ChangeOffset = false;
+    [SerializeField] bool ChangeOffsetY = false;
     [SerializeField] float CameraOffsetY;
-    [SerializeField] bool ChangeDeadZone = false;
+    [SerializeField] bool ChangeDeadZoneY = false;
     [SerializeField] float CameraDeadZoneY;
+    [SerializeField] bool ChangeOffsetX = false;
+    [SerializeField] float CameraOffsetX;
+    [SerializeField] bool ChangeDeadZoneX = false;
+    [SerializeField] float CameraDeadZoneX;
+    [SerializeField] float speed = 0.4f;
 
-    private float baseOffset;
-    private float baseDeadZone;
+    private float baseOffsetY;
+    private float baseDeadZoneY;
+    private float baseOffsetX;
+    private float baseDeadZoneX;
 
-    private float targetOffset;
-    private float targetDeadZone;
+    private float targetOffsetY;
+    private float targetDeadZoneY;
+    private float targetOffsetX;
+    private float targetDeadZoneX;
 
     private CinemachineVirtualCamera camera;
     // Start is called before the first frame update
     void Start()
     {
         camera = GameManager.Instance.GetCamera();
-        baseOffset = camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenY;
-        targetOffset = baseOffset;
-        baseDeadZone = camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_DeadZoneHeight;
-        targetDeadZone = baseDeadZone;
+        baseOffsetY = camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenY;
+        targetOffsetY = baseOffsetY;
+        baseDeadZoneY = camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_DeadZoneHeight;
+        targetDeadZoneY = baseDeadZoneY;
+        baseOffsetX = camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX;
+        targetOffsetX = baseOffsetX;
+        baseDeadZoneX = camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_DeadZoneWidth;
+        targetDeadZoneX = baseDeadZoneX;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (ChangeDeadZone)
+        if (ChangeDeadZoneY)
         {
-            float lerpedDeadZone = Mathf.Lerp(camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_DeadZoneHeight, targetDeadZone, 0.1f * Time.deltaTime);
+            float lerpedDeadZone = Mathf.Lerp(camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_DeadZoneHeight, targetDeadZoneY, speed * Time.deltaTime);
             camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_DeadZoneHeight = lerpedDeadZone;
         }
-        if (ChangeOffset)
+        if (ChangeOffsetY)
         {
-            float lerpedOffset = Mathf.Lerp(camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenY, targetOffset, 0.1f * Time.deltaTime);
+            float lerpedOffset = Mathf.Lerp(camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenY, targetOffsetY, speed * Time.deltaTime);
+            camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenY = lerpedOffset;
+        }
+        if (ChangeDeadZoneX)
+        {
+            float lerpedDeadZone = Mathf.Lerp(camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_DeadZoneWidth, targetDeadZoneX, speed * Time.deltaTime);
+            camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_DeadZoneHeight = lerpedDeadZone;
+        }
+        if (ChangeOffsetX)
+        {
+            float lerpedOffset = Mathf.Lerp(camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenY, targetOffsetX, speed * Time.deltaTime);
             camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenY = lerpedOffset;
         }
     }
@@ -48,13 +71,21 @@ public class CameraTrigger : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            if (ChangeDeadZone)
+            if (ChangeDeadZoneY)
             {
-                targetDeadZone = CameraDeadZoneY;
+                targetDeadZoneY = CameraDeadZoneY;
             }
-            if (ChangeOffset)
+            if (ChangeOffsetY)
             {
-                targetOffset = CameraOffsetY;
+                targetOffsetY = CameraOffsetY;
+            }
+            if (ChangeDeadZoneX)
+            {
+                targetDeadZoneX = CameraDeadZoneX;
+            }
+            if (ChangeOffsetX)
+            {
+                targetOffsetX = CameraOffsetX;
             }
         }
     }
@@ -63,13 +94,21 @@ public class CameraTrigger : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            if (ChangeDeadZone)
+            if (ChangeDeadZoneY)
             {
-                targetDeadZone = baseDeadZone;
+                targetDeadZoneY = baseDeadZoneY;
             }
-            if (ChangeOffset)
+            if (ChangeOffsetY)
             {
-                targetOffset = baseOffset;
+                targetOffsetY = baseOffsetY;
+            }
+            if (ChangeDeadZoneX)
+            {
+                targetDeadZoneX = baseDeadZoneX;
+            }
+            if (ChangeOffsetX)
+            {
+                targetOffsetX = baseOffsetX;
             }
         }
     }
