c5f888c384d1b0e5cc44c16d0f7b341867b57f76
mazafrav <mazafrav@gmail.com>
2024-07-10 13:21:38 +0200

    Added animated schema for lvl4

--- Assets/Scripts/MapElements/PopUpText.cs
+++ Assets/Scripts/MapElements/PopUpText.cs
@@ -10,16 +10,29 @@ public class PopUpText : MonoBehaviour
     [SerializeField]
     protected TextMeshProUGUI popUpText;
     protected Color newColor;
+    protected Color newColorSprite;
+
+    private SpriteRenderer sprite;
 
     // Update is called once per frame
     protected virtual void Start()
     {
         newColor = popUpText.color;
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        if (sprite != null)
+        {
+            newColorSprite = sprite.color;
+        }
+
     }
 
     protected virtual void Update()
     {
         popUpText.color = Color.Lerp(popUpText.color, newColor, lerpSpeed*Time.deltaTime);
+        if (sprite != null )
+        {
+            sprite.color = Color.Lerp(sprite.color, newColorSprite, lerpSpeed * Time.deltaTime);
+        }
     }
 
     protected void OnTriggerEnter2D(Collider2D other)
@@ -27,16 +40,28 @@ public class PopUpText : MonoBehaviour
         if (other.CompareTag("Player") || other.CompareTag("Enemy"))
         {
             newColor.a = 1.0f;
+            if (sprite != null)
+            {
+                newColorSprite.a = 1.0f;
+            }
         }
     }
 
     public void ActivateText()
     {
         newColor.a = 1.0f;
+        if (sprite != null)
+        {
+            newColorSprite.a = 1.0f;
+        }
     }
 
     public void DeactivateText()
     {
         newColor.a = 0f;
+        if (sprite != null)
+        {
+            newColorSprite.a = 0f;
+        }
     }
 }
