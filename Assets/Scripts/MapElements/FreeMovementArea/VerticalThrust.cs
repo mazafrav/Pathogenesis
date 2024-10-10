181454eb844487d83524d01dc7a55e083e63c708
Felipe Cuadra Plaza <fcuadra@ucm.es>
2024-10-10 19:55:05 +0200

    Added Gizmo and changed hierarchy in Vertical Thrust prefab

--- Assets/Scripts/MapElements/FreeMovementArea/VerticalThrust.cs
+++ Assets/Scripts/MapElements/FreeMovementArea/VerticalThrust.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class VerticalThrust : MonoBehaviour
@@ -7,18 +8,7 @@ public class VerticalThrust : MonoBehaviour
     [SerializeField] private float thrust = 20.0f;
     [SerializeField] private GameObject thrustBlocking;
 
-    public bool CanThrust { get; set; } = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {   
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
+   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         ActivateThrustBlocking(false);
@@ -32,17 +22,11 @@ public class VerticalThrust : MonoBehaviour
             playerLocomotion.DisableFreeMovement();
             rb.AddForce(new Vector3(0, 1) * thrust, ForceMode2D.Impulse);
         }
-
-
-
-        CanThrust = true;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        ActivateThrustBlocking(true);
-
-        Invoke("DeactivateCanThrust", 0.5f);
+        ActivateThrustBlocking(true);       
     }
 
     private void ActivateThrustBlocking(bool isActive)
@@ -52,34 +36,12 @@ public class VerticalThrust : MonoBehaviour
         thrustBlocking.GetComponent<ThrustBlocking>().enabled = isActive;        
     }
 
-    void DeactivateCanThrust()
+
+    private void OnDrawGizmos()
     {
-        CanThrust = false;
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(transform.position, transform.position + transform.up * 2.0f);
+
     }
-
-    //private void ApplyThrust(PlayerController playerController, Collider2D collision)
-    //{
-
-    //    Vector2 dir = new Vector2(playerController.GetDeltaX(), playerController.GetDeltaY());
-    //    if (dir.x > 0.0f || dir.x < 0.0f)
-    //    {
-    //        playerController.enabled = false;
-    //        hasDisabledControls = true;
-    //    }
-    //    //collision.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1) * 25, ForceMode2D.Impulse);
-
-    //    collision.GetComponent<Rigidbody2D>().velocity = new Vector2(30, 0);
-
-
-    //    //else if (applyThrustOverTheFloor)
-    //    //{
-    //    //    Vector2 dir = new Vector2(playerController.GetDeltaX(), playerController.GetDeltaY());
-    //    //    if (dir.x > 0.0f || dir.x < 0.0f)
-    //    //    {
-    //    //        playerController.enabled = false;
-    //    //        hasDisabledControls = true;
-    //    //    }
-    //    //    collision.GetComponent<Rigidbody2D>().AddForce(dir * thrust, ForceMode2D.Impulse);
-    //    //}
-    //}
 }
